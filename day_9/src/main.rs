c50511bddf9e4ac65fed5a00561a1dfd6a2702cf
namkwa <h.houis1507@gmail.com>
2022-12-12 16:55:36 +0100

    day_9

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 fn main() {
     let input = fs::read_to_string("day_9/src/input.txt").unwrap();
 
@@ -6,4 +6,66 @@ fn main() {
     star1(input);
 }
 
-fn star1(input: Vec<&str>) {}
+fn star1(input: Vec<&str>) {
+    let mut visited_positions: HashSet<(i32, i32)> = HashSet::new();
+    let mut current_x = 0;
+    let mut current_y = 0;
+    let mut current_direction: char = input.get(0).unwrap().chars().nth(0).unwrap();
+    let mut current_orientation: Orientation = get_orientation(current_direction);
+    for line in input {
+        println!("{} {:?}", line, (current_x, current_y));
+        if current_orientation != get_orientation(line.chars().nth(0).unwrap()) {
+            current_orientation = get_orientation(line.chars().nth(0).unwrap());
+            if current_orientation == Orientation::Horizontal {
+                match current_direction {
+                    'U' => {
+                        current_x -= 1;
+                        current_direction = 'U';
+                    }
+                    'D' => {
+                        current_x += 1;
+                        current_direction = 'D';
+                    }
+                    _ => panic!(),
+                }
+                continue;
+            } else {
+                match current_direction {
+                    'R' => {
+                        current_x += 1;
+                        current_direction = 'R';
+                    }
+                    'L' => {
+                        current_x -= 1;
+                        current_direction = 'L';
+                    }
+                    _ => panic!(),
+                }
+                continue;
+            }
+        }
+        match line.chars().nth(0).unwrap() {
+            'R' => current_x += line.chars().nth(2).unwrap().to_digit(10).unwrap() as i32,
+            'L' => current_x -= line.chars().nth(2).unwrap().to_digit(10).unwrap() as i32,
+            'U' => current_y += line.chars().nth(2).unwrap().to_digit(10).unwrap() as i32,
+            'D' => current_y -= line.chars().nth(2).unwrap().to_digit(10).unwrap() as i32,
+            _ => panic!(),
+        }
+        visited_positions.insert((current_x, current_y));
+    }
+    println!("{}", visited_positions.len());
+}
+
+fn get_orientation(orientation: char) -> Orientation {
+    match orientation {
+        'R' | 'L' => return Orientation::Horizontal,
+        'U' | 'D' => return Orientation::Vertical,
+        _ => return Orientation::Vertical,
+    }
+}
+
+#[derive(PartialEq)]
+enum Orientation {
+    Horizontal,
+    Vertical,
+}
