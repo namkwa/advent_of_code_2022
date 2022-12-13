6162f9055fc08cec7439d1a07c848bc6cd918bea
namkwa <h.houis1507@gmail.com>
2022-12-13 16:29:31 +0100

    start day_12

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -2,13 +2,13 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("day_12/src/input.txt").unwrap();
 
-    let input: Vec<Vec<char>> = input
+    let input: Vec<Vec<i32>> = input
         .lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>();
-    println!("{:?}", input);
+        .map(|line| line.chars().map(|x| x as i32 - 96).collect::<Vec<i32>>())
+        .collect::<Vec<Vec<i32>>>();
     star1(input);
-    //star2(&input);
 }
 
-fn star1(input: Vec<Vec<char>>) {}
+fn star1(input: Vec<Vec<i32>>) {}
+
+fn get_next_step() {}
